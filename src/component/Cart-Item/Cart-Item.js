aa4fbad9fde14b0dca1104e722703ac1742ed1ad
obidullah124 <obidullahbinmasud124@gmail.com>
2022-10-04 03:05:33 +0600

    many many style added

---
+++ src/component/Cart-Item/Cart-Item.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Cart-Item.css'
+const CartItem = (props) => {
+   const { img, name, time } = props.cart;
+   return (
+      <div className='cart-item'>
+         <img src={img} alt="images" />
+         <div className='cart-item-text'>
+            <h2>{name}</h2>
+            <p>Onsectetur adipisicing elit. Maiores, deserunt quasi. Eius quisquam recusandae accusantium?</p>
+         </div>
+
+         <div className='cart-item-btn'>
+            <button>Add to list </button>
+         </div>
+      </div>
+   );
+};
+
+export default CartItem;
--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react';
-
+import CartItem from '../Cart-Item/Cart-Item';
+import './Cart.css'
 const Cart = () => {
-   const {carts, setCarts} = useState([]);
+   const [carts, setCarts] = useState([]);
 
-   useEffect(()=> {
+   useEffect(() => {
       fetch('fakeAPI.json')
-      .then(res=> res.json())
-      .then(data => console.log(data))
-   }, [])
+         .then(res => res.json())
+         .then(data => setCarts(data))
+   }, []);
+
+
    return (
-      <div>
-         
+      <div className='cart-container'>
+         <div className='item-container'>
+            {
+               carts.map(cart => <CartItem cart={cart} key={cart.id}></CartItem>)
+            }
+         </div>
+
+         <div>
+            <h2>Founder compo</h2>
+         </div>
+
       </div>
    );
 };
