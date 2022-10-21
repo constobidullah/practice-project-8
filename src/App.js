e193ca7a71b109da6c8a16847b609e2d8a096a81
obidullah124 <obidullahbinmasud124@gmail.com>
2022-10-21 16:06:22 +0600

    One question answer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './component/header/Header';
 import Cart from './component/cart/Cart';
+import QuestionAnswer from './component/QuestionAnswer/QuestionAnswer';
 
 function App() {
   return (
     <div className='app'>
       <Header></Header>
       <Cart></Cart>
+      <QuestionAnswer></QuestionAnswer>
     </div>
   );
 }
---
+++ src/component/QuestionAnswer/QuestionAnswer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QuestionAnswer = () => {
+    return (
+        <div>
+            <h3>How does React Works ?</h3>
+            <p>Let’s say one of your friends posted a photograph on Facebook. Now you go and like the image and then you started checking out the comments too. Now while you are browsing over comments you see that the likes count has increased by 100, since you liked the picture, even without reloading the page. This magical count change is because of ReactJS.
+                React is a declarative, efficient, and flexible JavaScript library for building user interfaces. ‘V’ denotes the view in MVC. ReactJS is an open-source, component-based front end library responsible only for the view layer of the application. It is maintained by Facebook.</p>
+        </div>
+    );
+};
+
+export default QuestionAnswer;
